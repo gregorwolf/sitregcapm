9c92202462a0d7b282a47e39b93faea0fb29721c
Gregor Wolf <gregor.wolf@gmail.com>
2019-05-27 16:01:59 +0200

    Fix i18n issue

--- srv/app.js
+++ srv/app.js
@@ -2,6 +2,7 @@ const app = require('express')()
 const helmet = require('helmet')
 const compression = require('compression')
 const cds = require('@sap/cds')
+cds.options = { kind: 'sqlite' }
 
 app.use(helmet())
 app.use(compression({ threshold: '512b' }))
